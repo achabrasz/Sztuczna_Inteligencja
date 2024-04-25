c01f1f2cff040cfdc618fecd466c8449a1e698c7
achabrasz <adam.chabraszewski@gmail.com>
2024-04-25 23:29:36 +0200

    Lab5

--- lab5/k_means.py
+++ lab5/k_means.py
@@ -7,34 +7,38 @@ def initialize_centroids_forgy(data, k):
 
 def initialize_centroids_kmeans_pp(data, k):
     # TODO implement kmeans++ initizalization
-    centroids  = []
-    for i in range(k):
-        if i == 0:
-            centroids.append(np.random.choice(data.shape[0], 1, replace=False))
-        else:
-            distances = np.zeros(data.shape[0])
-            for j in range(data.shape[0]):
-                distances[j] = np.sqrt(np.sum(abs(data - data[j])))
-            prob = distances / np.sum(distances)
-            centroids.append(np.random.choice(data.shape[0], 1, replace=False, p=prob))
-    return centroids
+    centroids = []
+    centroids.append(data[np.random.randint(data.shape[0])])
+    for _ in range(1, k):
+        distances = []
+        for point in data:
+            distances.append(np.min([np.sqrt(np.sum(abs(point - centroid)**2)) for centroid in centroids]))
+        distances = np.array(distances)
+        probabilities = distances ** 2 / np.sum(distances ** 2)
+        new_centroid = data[np.random.choice(data.shape[0], p=probabilities)]
+        centroids.append(new_centroid)
+
+    return np.array(centroids)
 
 def assign_to_cluster(data, centroid):
     # TODO find the closest cluster for each data point
-    cluster = np.zeros(data.shape[0])
+    cluster = np.zeros(data.shape[0], dtype=int)
     for i in range(data.shape[0]):
         distances = np.zeros(len(centroid))
         for j in range(len(centroid)):
-            distances[j] = np.sqrt(np.sum(abs(data[i] - centroid[j])))
+            distances[j] = np.sqrt(np.sum(abs(data[i] - centroid[j])**2))
         cluster[i] = np.argmin(distances)
     return cluster
 
 def update_centroids(data, assignments):
+    centroid = []
     # TODO find new centroids based on the assignments
-
-    return None
+    for i in np.unique(assignments):
+        centroid.append(np.mean(data[assignments==i], axis=0))
+    return centroid
 
 def mean_intra_distance(data, assignments, centroids):
+    centroids = np.array(centroids)
     return np.sqrt(np.sum((data - centroids[assignments, :])**2))
 
 def k_means(data, num_centroids, kmeansplusplus= False):
